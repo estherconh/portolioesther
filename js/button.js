0893e3ddaafb04e6e366b97ceb387c7520100ffa
Esther Marquez <str.marquez@gmail.com>
2017-04-16 15:13:37 +0200

    cambios

--- js/button.js
+++ js/button.js
@@ -3,9 +3,4 @@ $("button").click(function() {
   $(".content").toggleClass("show")
 });
 
-$("nav").click(function(){
-  $(".content").toggleClass('show');
-  $(".burger").toggleClass("focus");
-});
-
 document.querySelector("html").classList.add("js");
--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,3 @@
-$("button").click(function() {
-  $(".burger").toggleClass("focus");
-  $(".content").toggleClass("show")
-});
-
 $("nav").click(function(){
   $(".content").toggleClass('show');
   $(".burger").toggleClass("focus");
