c365a0036cd84072cd724d8e487497833eafbd85
Esther Marquez <str.marquez@gmail.com>
2017-04-24 12:38:29 +0200

    Updates

---
+++ js/button.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("button").click(function() {
+      $(".burger").toggleClass("focus");
+      $(".content").toggleClass("show")
+    });
+  });
---
+++ js/menu.js
@@ -0,0 +1,11 @@
+$("button").click(function() {
+  $(".burger").toggleClass("focus");
+  $(".content").toggleClass("show")
+});
+
+$("nav").click(function(){
+  $(".content").toggleClass("show");
+  $(".burger").toggleClass("focus");
+});
+
+document.querySelector("html").classList.add("js");
